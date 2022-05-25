cebb132e709f15ff4f574f809ef1de55d9a7180c
soos88885 <55650250+soos88885@users.noreply.github.com>
2022-05-25 16:04:38 +0200

    Exmple monogame.extended app

---
+++ StrategicGame/Raindrop.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StrategicGame
+{
+    public class Raindrop
+    {
+        public Vector2 Velocity;
+        public float Size = 3;
+    }
+}
