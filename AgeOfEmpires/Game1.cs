5ba78e2d7c5af749379bb15a6b5299f60b588c84
Matteo La Barbera <71218200+Matts-cloud@users.noreply.github.com>
2022-06-03 20:59:47 +0200

    Cleaned up files

--- AgeOfEmpires/Game1.cs
+++ AgeOfEmpires/Game1.cs
@@ -23,6 +23,7 @@ namespace AgeOfEmpires
         private OrthographicCamera _camera;
         private Vector2 _cameraPosition;
 
+
         private readonly ScreenManager _screenManager;
 
 
@@ -41,12 +42,13 @@ namespace AgeOfEmpires
             Components.Add(_screenManager);
         }
 
+       
         protected override void Initialize()
         {
+
             var viewportadapter = new BoxingViewportAdapter(Window, GraphicsDevice, 800, 600);
             _camera = new OrthographicCamera(viewportadapter);
-            _cameraPosition = new Vector2(_graphics.PreferredBackBufferWidth/2, _graphics.PreferredBackBufferHeight/2+100);
-
+            _cameraPosition = new Vector2(_graphics.PreferredBackBufferWidth / 2, _graphics.PreferredBackBufferHeight / 2 + 100);
             base.Initialize();
             LoadMainMenu();
         }
@@ -60,10 +62,8 @@ namespace AgeOfEmpires
 
         protected override void Update(GameTime gameTime)
         {
-           
-            
-           MoveCamera(gameTime);
             _camera.LookAt(_cameraPosition);
+            MoveCamera(gameTime);
 
             KeyboardState keyboard = Keyboard.GetState();
             if (keyboard.IsKeyDown(Keys.Escape))
@@ -80,6 +80,25 @@ namespace AgeOfEmpires
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             base.Draw(gameTime);
+        }
+
+        private void LoadMainMenu()
+        {
+            _screenManager.LoadScreen(new MainMenu(this));
+        }
+
+        private void LoadGamePlay()
+        {
+            _screenManager.LoadScreen(new GamePlay(this));
+        }
+
+        private void MoveCamera(GameTime gameTime)
+        {
+            var speed = 400;
+            var seconds = gameTime.GetElapsedSeconds();
+            var movementDirection = GetMovementDirection();
+            _cameraPosition += speed * movementDirection * seconds;
+            Debug.WriteLine(movementDirection);
         }
 
         private Vector2 GetMovementDirection()
@@ -106,16 +125,19 @@ namespace AgeOfEmpires
 
             var state = Mouse.GetState();
             Debug.WriteLine("Mouse: " + state.X);
-            if (state.X < 50) {
+            if (state.X < 50)
+            {
                 movementDirection -= Vector2.UnitX;
             }
-            if (state.X > _graphics.PreferredBackBufferWidth - 50) {
+            if (state.X > _graphics.PreferredBackBufferWidth - 50)
+            {
                 movementDirection += Vector2.UnitX;
             }
-            if (state.Y < 50 ) {
+            if (state.Y < 50)
+            {
                 movementDirection -= Vector2.UnitY;
             }
-            if (state.Y > _graphics.PreferredBackBufferHeight-50)
+            if (state.Y > _graphics.PreferredBackBufferHeight - 50)
             {
                 movementDirection += Vector2.UnitY;
             }
@@ -127,25 +149,6 @@ namespace AgeOfEmpires
             }
 
             return movementDirection;
-        }
-
-        private void MoveCamera(GameTime gameTime)
-        {
-            var speed = 400;
-            var seconds = gameTime.GetElapsedSeconds();
-            var movementDirection = GetMovementDirection();
-            _cameraPosition += speed * movementDirection * seconds;
-            Debug.WriteLine(movementDirection);
-        }
-
-        private void LoadMainMenu()
-        {
-            _screenManager.LoadScreen(new MainMenu(this));
-        }
-
-        private void LoadGamePlay()
-        {
-            _screenManager.LoadScreen(new GamePlay(this));
         }
 
 
--- AgeOfEmpires/States/GamePlay.cs
+++ AgeOfEmpires/States/GamePlay.cs
@@ -1,17 +1,29 @@
-﻿using Microsoft.Xna.Framework;
-using MonoGame.Extended.Screens;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AgeOfEmpires;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using MonoGame.Extended.Entities;
 using MonoGame.Extended.Tiled;
 using MonoGame.Extended.Tiled.Renderers;
+using MonoGame.Extended.ViewportAdapters;
+using MonoGame.Extended.Screens;
+using MonoGame.Extended.Screens.Transitions;
+using System;
+using System.Diagnostics;
+using AgeOfEmpires.States;
 
 namespace AgeOfEmpires.States
 {
     class GamePlay : GameScreen
     {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
         private TiledMap _tiledMap;
         private TiledMapRenderer _tiledMapRenderer;
+
+       
 
         private new Game1 Game => (Game1)base.Game;
 
@@ -20,10 +32,18 @@ namespace AgeOfEmpires.States
         public GamePlay(Game1 game) : base(game)
         {
             
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        public override void Initialize()
+        {
+           
+            base.Initialize();
         }
 
         public override void LoadContent()
         {
+
             _tiledMap = Content.Load<TiledMap>("editedTilesSet");
             _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
             base.LoadContent();
@@ -35,8 +55,13 @@ namespace AgeOfEmpires.States
 
         public override void Update(GameTime gameTime)
         {
-
+            
+            
             _tiledMapRenderer.Update(gameTime);
         }
+
+       
+
+       
     }
 }
